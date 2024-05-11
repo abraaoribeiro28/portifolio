f66917eb3e39fdb4c0bf552d982df1c9f745c64f
Abraão Ribeiro <abraaobeiro@gmail.com>
2024-05-11 17:54:16 -0300

    Adiciona componente do footer do painel

---
+++ resources/views/components/layout/footer.blade.php
@@ -0,0 +1,21 @@
+<footer class="py-4">
+    <div class="w-full px-6 mx-auto">
+        <div class="flex flex-wrap items-center -mx-3 lg:justify-between">
+            <div class="w-full max-w-full px-3 mt-0 mb-6 shrink-0 lg:mb-0 lg:w-1/2 lg:flex-none">
+                <div class="leading-normal text-center copyright text-sm text-slate-500 lg:text-left">
+                    © 2024,
+                    <span class="font-semibold text-slate-700 dark:text-white">Company Name</span>
+                </div>
+            </div>
+            <div class="w-full max-w-full px-3 mt-0 shrink-0 lg:w-1/2 lg:flex-none">
+                <ul class="flex flex-wrap justify-center pl-0 mb-0 list-none lg:justify-end">
+                    <li class="nav-item">
+                        <span class="block px-4 pt-0 pb-1 font-normal transition-colors ease-soft-in-out text-sm text-slate-500">
+                            Versão do sistema: 0.0.0
+                        </span>
+                    </li>
+                </ul>
+            </div>
+        </div>
+    </div>
+</footer>
