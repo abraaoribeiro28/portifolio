709cde83fdd0ca134d6c4c881b6f6a95af02f28c
Abraão Ribeiro <abraaobeiro@gmail.com>
2024-05-15 19:14:36 -0300

    Implementando view e controller da tela de usuarios

---
+++ resources/views/admin/users/index.blade.php
@@ -0,0 +1,15 @@
+<x-app-layout>
+    <x-slot name="title">
+        Usuários
+    </x-slot>
+
+    <div class="py-12">
+        <div class="mx-auto">
+            <div class="bg-white dark:bg-gray-800 overflow-hidden shadow-sm sm:rounded-lg">
+                <div class="p-6 text-gray-900 dark:text-gray-100">
+                    Listagem de usuários
+                </div>
+            </div>
+        </div>
+    </div>
+</x-app-layout>
